670889a735010ee6fab8a29834fe24f7c255b75e
Luca Tejada <lucaatejada@icloud.com>
2022-10-11 13:58:09 -0300

    13:58

--- vistas/paginas/error404.php
+++ vistas/paginas/error404.php
@@ -1,5 +1,5 @@
 <div class="text-center">   
-<H2> ERROR 404</H2>
+<H2> ERROR 404 </H2>
 <p> Pagina no encontrada :)</p>
-<a href="index.php?pagina=registro" class="btn btn-primary"> Volver</a>
+<a href="index.php?pagina=registro" class="btn btn-primary"> Volver </a>
 </div>
